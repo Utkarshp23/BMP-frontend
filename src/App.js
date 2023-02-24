61732fec63e566c5aea0eb7f65b997e121927c27
Utkarshp23 <utkarshpawar2332@gmail.com>
2023-02-24 16:58:26 +0530

    imported router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      {/* <a className="btn btn-primary"
+      <a className="btn btn-primary"
         data-bs-toggle="collapse"
         href="#collapseExample"
         role="button"
         aria-expanded="false"
         aria-controls="collapseExample">
         Bootstrap button
-      </a> */}
+      </a>
     </div>
   );
 }
