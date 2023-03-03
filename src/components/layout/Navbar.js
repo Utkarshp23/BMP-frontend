3c78b3d7f50a249daff0900f5c9b668b4c19a85a
Utkarshp23 <utkarshpawar2332@gmail.com>
2023-03-03 20:20:11 +0530

    user state management using redux added

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,29 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.scss';
-import {BsBuilding} from 'react-icons/bs'
+import { BsBuilding } from 'react-icons/bs';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout } from '../slice';
 
 const Navbar = () => {
+  const myState = useSelector((state) => state.logged);
+  console.log(myState.loggedIn);
+
   return (
     <nav className='nav_container navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 mb-5 bg-white rounded'>
       <Link className='navbar-brand' to='/'>
-        <BsBuilding/>
+        <BsBuilding />
         <h5>Book My Property</h5>
       </Link>
 
       <div className='nav_buttons'>
-        <ul className='navbar-nav mr-auto'>
-          <li className='nav-item'>
-            <Link className='nav-link' to='/login'>
-              Login
-            </Link>
-          </li>
-          <li className='nav-item'>
-            <Link className='nav-link' to='/signup'>
-              Signup
-            </Link>
-          </li>
-        </ul>
+        {!myState.loggedIn ? (
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/login'>
+                Login
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/signup'>
+                Signup
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/logout'>
+                Logout
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
     </nav>
   );
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './components/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   // </React.StrictMode>
 );
