fea54f1f05d920d673d3bf00cc7bc625cc2f2e58
Mhaske Shubham <mhaskeshubham1200@gmail.com>
2023-03-09 18:21:37 +0530

    no change

--- src/components/owner/OwnerHomepage.js
+++ src/components/owner/OwnerHomepage.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React,{useEffect} from "react";
 export default function OwnerHomepage()
 {
+    useEffect(() => {
+        var userSel = document.getElementById('addproperty');
+      
+    
+        fetch('http://localhost:8080/getproperty')
+          .then((res) => res.json())
+          .then((res) => {
+            console.log(res);
+    
+            res.map((v) => {
+              var opt = document.createElement('table');
+              opt.value = v.ftypeid;
+              opt.textContent = v.ftype;
+              userSel.appendChild(opt);
+            });
+          });
+      }, []);
+    
+
+
     return(
-        <h1>Welcome to Owner Page</h1>
+        <div>
+            <h1>Welcome to Owner Page</h1>
+            <input type="button" id="addproperty" value="Add Property"
+            onClick={(e) =>
+            <table>
+                <tr>
+                    <td>
+
+                    </td>
+                </tr>
+            </table> }/>
+        </div>
+        
     )
 }
